444b630cdfb1bb98987ae2d4087b5c0d30d593c9
romreed <romreed@ya.ru>
2016-09-28 00:06:53 +0300

    подсветка кода для ларавел в шторм

--- config/app.php
+++ config/app.php
@@ -225,6 +225,11 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
+        /*
+         *
+         * подсветка кода для ларавел
+         */
+        Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider::class,
 
     ],
 
